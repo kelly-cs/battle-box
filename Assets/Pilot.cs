5ec59cd46ed86996fe5d4caed3bf9e1261f6bf26
Likai Kuroi <likai31197@gmail.com>
2020-04-18 02:42:28 -0400

    Ship and Pilot box w/ controls

---
+++ Assets/Pilot.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pilot : MonoBehaviour 
+{
+    public float moveSpeed = 200f;
+    public Rigidbody2D _shipRigidBody;
+    private Rigidbody2D _rigidBody;
+
+    private void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // do rigidbody stuff
+    private void LateUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal_Pilot");
+        float verticalInput = Input.GetAxis("Vertical_Pilot");
+        Vector2 force = new Vector2(horizontalInput, verticalInput) * moveSpeed * Time.deltaTime;
+        _rigidBody.velocity = force + _shipRigidBody.velocity;
+
+        //transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * moveSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    public float moveSpeed = 20f;
+    private Rigidbody2D _rigidBody;
+
+    private void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal_Ship");
+        float verticalInput = Input.GetAxis("Vertical_Ship");
+        Vector2 force = new Vector2(horizontalInput, verticalInput) * moveSpeed * Time.deltaTime;
+        _rigidBody.velocity = force;
+
+        //transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * moveSpeed * Time.deltaTime);
+    }
+}
